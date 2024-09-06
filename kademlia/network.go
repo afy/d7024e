d573fefdd989ddeb2e4135294d5a02650a21cf77
afy <hannes.furhoff@hotmail.com>
2024-09-06 14:31:29 +0200

    Begin listener

--- kademlia/network.go
+++ kademlia/network.go
@@ -1,10 +1,28 @@
 package kademlia
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
 type Network struct {
 }
 
+func HandleCallback(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("CB")
+}
+
 func Listen(ip string, port int) {
-	// TODO
+	go func() {
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "8008"
+		}
+		http.HandleFunc("/", HandleCallback)
+		log.Fatal(http.ListenAndServe(":"+port, nil))
+	}()
 }
 
 func (network *Network) SendPingMessage(contact *Contact) {
